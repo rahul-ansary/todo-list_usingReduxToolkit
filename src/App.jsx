ff60f13c4ebcda24ddb73c888a5387c2b3fed410
rahul-ansary <rahulansary38@gmail.com>
2024-01-26 17:29:51 +0530

    Todo-list

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useDispatch } from 'react-redux'
+import './App.css'
+import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
+import { bindActionCreators } from 'redux';
+import todoReducer, { addTodo, todoFinished, deleteTodo, editTodo } from './slices/todoSlice';
+
+function App() {
+
+  const dispatch = useDispatch();
+  const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, todoFinished }, dispatch);
+
+
+  return (
+    <>
+      <AddTodo addTodo={actions.addTodo} />
+      <TodoList
+        deleteTodo={actions.deleteTodo}
+        editTodo={actions.editTodo}
+        todoFinished={actions.todoFinished}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/store.js
@@ -0,0 +1,14 @@
+
+// reducers import 
+
+import todoReducer from "./slices/todoSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+                        reducer: {
+                            todo: todoReducer
+                        },
+    devTools: process.env.NODE_ENV !== 'production',
+                    });
+
+export default store;
